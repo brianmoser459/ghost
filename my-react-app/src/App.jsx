2819f587ab33fbdf31320d56af8df21b77c0b291
brianmoser459 <anikbusiness008@gmail.com>
2025-02-08 15:07:02 +0530

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white text-3xl font-bold p-10 text-center">
+      Jayanta Sarkar is gay
+    </div>
+  );
+}
+
+export default App;
